427e809fbaf5b14b20793c719dc33be8761fdd98
Fuad Süleymanlı <suleymanli95@hotmail.com>
2021-06-12 04:10:44 +0400

    Upload to Github

---
+++ js/main.js
@@ -0,0 +1,43 @@
+AOS.init()
+
+
+// Ehtiyac olsa  content.json dan məlumatları çəkib  #main divinin içinə yerləşdirmək olar
+
+// let main = document.querySelector("#main")
+
+// fetch('../json/content.json')
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     let newData = data.timeline
+
+//   newData.map((e,index)=>{
+//     index=index+1
+//     console.log(e.caption)
+// if (index%2==0){
+// main.innerHTML=main.innerHTML+`<div class="timeline-item" >
+// <div class="timeline-circle"></div>
+// <div class="timeline-content">
+//     <h2>${e.caption}</h2>
+//     <div class="date">${e.date}</div>
+//     <p>${e.content}</p>
+
+// </div>`
+
+// }else{
+//     main.innerHTML=main.innerHTML+`<div class="timeline-item" >
+//     <div class="timeline-circle"></div>
+//     <div class="timeline-content">
+//         <h2>${e.caption}</h2>
+//         <div class="date">${e.date}</div>
+//         <p>${e.content}</p>
+    
+//     </div>`
+
+// }
+
+//   })
+
+//   });
+
